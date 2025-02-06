ca6cc5cb6f93c4259880ac019d30b59624122057
AkifAycicek <akifaycicek@gmail.com>
2025-02-06 16:03:56 +0300

    refactor(migrations): streamline trigger creation syntax for user and permissions management

--- supabase/migrations/005_init_triggers.sql
+++ supabase/migrations/005_init_triggers.sql
@@ -1,31 +1,23 @@
 -- Create or replace trigger for new users
 DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
 
-CREATE TRIGGER on_auth_user_created
-AFTER INSERT ON auth.users FOR EACH ROW
-EXECUTE FUNCTION private.handle_new_user ();
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION private.handle_new_user ();
 
 -- Create or replace trigger for updated users
 DROP TRIGGER IF EXISTS on_auth_user_updated ON auth.users;
 
-CREATE TRIGGER on_auth_user_updated
-AFTER
-UPDATE ON auth.users FOR EACH ROW
-EXECUTE FUNCTION private.handle_update_user ();
+CREATE TRIGGER on_auth_user_updated AFTER
+UPDATE ON auth.users FOR EACH ROW EXECUTE FUNCTION private.handle_update_user ();
 
 -- Create or replace trigger for user deletions
 DROP TRIGGER IF EXISTS on_auth_user_deleted ON auth.users;
 
-CREATE TRIGGER on_auth_user_deleted
-AFTER DELETE ON auth.users FOR EACH ROW
-EXECUTE FUNCTION private.handle_user_delete ();
+CREATE TRIGGER on_auth_user_deleted AFTER DELETE ON auth.users FOR EACH ROW EXECUTE FUNCTION private.handle_user_delete ();
 
 -- Create or replace trigger to watch changes on permissions
 DROP TRIGGER IF EXISTS permissions_trigger ON public.permissions;
 
-CREATE TRIGGER permissions_trigger
-AFTER INSERT
+CREATE TRIGGER permissions_trigger AFTER INSERT
 OR
 UPDATE
-OR DELETE ON public.permissions FOR EACH ROW
-EXECUTE FUNCTION private.manage_policies ();
+OR DELETE ON public.permissions FOR EACH ROW EXECUTE FUNCTION private.manage_policies ();
