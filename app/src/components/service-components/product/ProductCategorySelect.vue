28c7fe383c94b5dc9660a90dbd667f3e6e0b461e
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 01:04:12 +0300

    feat(product): add ProductCategorySelect component for category selection with loading state

---
+++ app/src/components/service-components/product/ProductCategorySelect.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { PRODUCT_CATEGORY_PROPS } from '@/services/models/ProductModel';
+import { useProductsStore } from '@/store/services/product';
+
+const routeLoading = inject('routeLoading', false);
+
+const productStore = useProductsStore();
+const productCategories = routeLoading?.value ? [] : await productStore.getCategories();
+</script>
+
+<template>
+   <MultiSelect
+      :filter="true"
+      :options="productCategories._data"
+      option-label="display_name"
+      option-value="display_name"
+      display="chip"
+   >
+      <template #value="{ value }">
+         <Badge
+            v-for="category in value"
+            :value="$t(category?.display_name || category)"
+            size="large"
+            :key="category"
+            v-bind="PRODUCT_CATEGORY_PROPS[category?.display_name || category]"
+         />
+      </template>
+      <template #option="{ option }">
+         <Badge
+            :value="$t(option?.display_name || option)"
+            size="large"
+            v-bind="PRODUCT_CATEGORY_PROPS[option?.display_name || option]"
+         />
+      </template>
+   </MultiSelect>
+</template>
