eadff2075ca523f73384a03a52abf29912a5ad33
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 00:23:40 +0300

    feat(KeywordSearchInput): add keyword search input component with model binding

---
+++ app/src/components/ui/CustomTable/KeywordSearchInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+const modelValue = defineModel('modelValue');
+</script>
+
+<template>
+   <IconField class="w-96 max-w-1/4">
+      <InputIcon>
+         <i class="pi pi-search" />
+      </InputIcon>
+      <InputText
+         v-keyfilter="{ pattern: /[a-zA-Z0-9\.\-\/\s]+/ }"
+         :placeholder="$t('keyword search')"
+         class="w-full"
+         v-model="modelValue"
+      />
+   </IconField>
+</template>
