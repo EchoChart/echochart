c21134e5ae76ad6c94f78ec8e6135dbe0ed08f90
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 01:05:08 +0300

    feat(stock): add StockVendorSelect component for vendor selection with fetching capabilities

---
+++ app/src/components/service-components/stock/StockVendorSelect.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useStocksStore } from '@/store/services/stock';
+
+const { getVendors } = useStocksStore();
+const vendors = await getVendors();
+</script>
+
+<template>
+   <Select
+      filter
+      :options="vendors._data"
+      option-label="display_name"
+      option-value="display_name"
+      editable
+   />
+</template>
