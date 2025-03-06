55b4083f7e1cffa5adbdd8c210e2e227d24724c1
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 00:33:23 +0300

    refactor(migrations): add indexes to improve query performance and optimize table structure

--- supabase/migrations/003_init_tables.sql
+++ supabase/migrations/003_init_tables.sql
@@ -6,10 +6,11 @@ CREATE TABLE
       phone TEXT UNIQUE,
       email TEXT UNIQUE,
       created_at TIMESTAMP DEFAULT NOW (),
-      updated_at TIMESTAMP,
       parent_id UUID REFERENCES public.tenants (id) ON DELETE CASCADE,
       CONSTRAINT unique_tenant_display_name UNIQUE (id, display_name)
    );
+CREATE INDEX tenants_display_name_idx ON public.tenants (display_name);
+CREATE INDEX tenants_created_at_idx ON public.tenants (created_at);
 
 -- Users
 CREATE TABLE
@@ -19,19 +20,21 @@ CREATE TABLE
       avatar_url TEXT,
       email TEXT NOT NULL UNIQUE,
       phone TEXT UNIQUE,
-      created_at TIMESTAMP DEFAULT NOW (),
-      updated_at TIMESTAMP
+      created_at TIMESTAMP DEFAULT NOW ()
    );
+CREATE INDEX users_email_idx ON public.users (email);
+CREATE INDEX users_created_at_idx ON public.users (created_at);
 
 -- Tenant Users
 CREATE TABLE
    IF NOT EXISTS public.tenants_users (
-      id UUID UNIQUE DEFAULT gen_random_uuid (),
-      tenant_id UUID NOT NULL REFERENCES public.tenants (id) ON DELETE CASCADE,
       user_id UUID NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
+      tenant_id UUID NOT NULL REFERENCES public.tenants (id) ON DELETE CASCADE,
       created_at TIMESTAMP DEFAULT NOW (),
       PRIMARY KEY (tenant_id, user_id)
    );
+CREATE INDEX tenants_users_user_id_idx ON public.tenants_users (user_id);
+CREATE INDEX tenants_users_tenant_id_idx ON public.tenants_users (tenant_id);
 
 -- Addresses
 CREATE TABLE
@@ -47,12 +50,14 @@ CREATE TABLE
       building_number TEXT,
       floor_number TEXT,
       door_number TEXT,
-      postal_code TEXT CHECK (postal_code ~'^\d{5}$'),
+      postal_code TEXT,
       phone TEXT,
       created_at TIMESTAMP DEFAULT NOW (),
-      updated_at TIMESTAMP,
       tenant_id UUID NOT NULL REFERENCES public.tenants (id) ON DELETE CASCADE
    );
+CREATE INDEX addresses_tenant_id_idx ON public.addresses (tenant_id);
+CREATE INDEX addresses_display_name_idx ON public.addresses (display_name);
+CREATE INDEX addresses_created_at_idx ON public.addresses (created_at);
 
 -- App Permissions
 CREATE TABLE
@@ -63,14 +68,15 @@ CREATE TABLE
       command permission_command NOT NULL,
       kind permission_kind NOT NULL,
       condition TEXT,
-      resource_condition TEXT,
+      resource_condition TEXT DEFAULT 'TRUE',
       description TEXT,
       error_message TEXT,
       throws_error BOOLEAN DEFAULT FALSE,
       bypass BOOLEAN DEFAULT FALSE,
-      created_at TIMESTAMP DEFAULT NOW (),
-      updated_at TIMESTAMP
+      created_at TIMESTAMP DEFAULT NOW ()
    );
+CREATE INDEX permissions_resource_name_idx ON public.permissions (resource_name);
+CREATE INDEX permissions_created_at_idx ON public.permissions (created_at);
 
 -- Roles
 CREATE TABLE
@@ -78,13 +84,14 @@ CREATE TABLE
       id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
       display_name TEXT NOT NULL,
       created_at TIMESTAMP DEFAULT NOW (),
-      updated_at TIMESTAMP,
       is_default BOOLEAN DEFAULT FALSE,
-      tenant_id UUID NOT NULL REFERENCES public.tenants (id) ON DELETE CASCADE,
+      tenant_id UUID REFERENCES public.tenants (id) ON DELETE CASCADE,
       CONSTRAINT unique_role_display_name UNIQUE (tenant_id, display_name)
    );
+CREATE INDEX roles_tenant_id_idx ON public.roles (tenant_id);
+CREATE INDEX roles_created_at_idx ON public.roles (created_at);
 
--- Permissions
+-- Role Permissions
 CREATE TABLE
    IF NOT EXISTS public.role_permissions (
       role_id UUID NOT NULL REFERENCES public.roles (id) ON DELETE CASCADE,
@@ -92,17 +99,19 @@ CREATE TABLE
       created_at TIMESTAMP DEFAULT NOW (),
       PRIMARY KEY (permission_id, role_id)
    );
+CREATE INDEX role_permissions_role_id_idx ON public.role_permissions (role_id);
+CREATE INDEX role_permissions_permission_id_idx ON public.role_permissions (permission_id);
 
 -- User Roles
 CREATE TABLE
    IF NOT EXISTS public.user_roles (
-      id UUID UNIQUE DEFAULT gen_random_uuid (),
       user_id UUID NOT NULL REFERENCES public.users (id) ON DELETE CASCADE,
       role_id UUID NOT NULL REFERENCES public.roles (id) ON DELETE CASCADE,
       created_at TIMESTAMP DEFAULT NOW (),
-      is_default BOOLEAN DEFAULT FALSE,
       PRIMARY KEY (user_id, role_id)
    );
+CREATE INDEX user_roles_user_id_idx ON public.user_roles (user_id);
+CREATE INDEX user_roles_role_id_idx ON public.user_roles (role_id);
 
 -- Tenants Table
 CREATE INDEX IF NOT EXISTS idx_tenants_display_name ON public.tenants (display_name);
