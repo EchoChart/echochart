c46c16fd5535c60b9b57362b14e1d733e17d3096
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 00:32:46 +0300

    refactor(supabaseAdminClient): standardize string quotes and improve formatting

--- supabase/functions/_shared/supabaseAdminClient.ts
+++ supabase/functions/_shared/supabaseAdminClient.ts
@@ -1,12 +1,12 @@
-import { createClient } from "jsr:@supabase/supabase-js@2.47.10";
+import { createClient } from 'jsr:@supabase/supabase-js@2.47.10';
 export const supabaseAdmin = () =>
-  createClient(
-    Deno.env.get("SUPABASE_URL") ?? "",
-    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
-    {
-      auth: {
-        autoRefreshToken: false,
-        persistSession: false,
-      },
-    }
-  );
+   createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
+      {
+         auth: {
+            autoRefreshToken: false,
+            persistSession: false
+         }
+      }
+   );
