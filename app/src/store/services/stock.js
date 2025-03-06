f91996a7985023e8efc61c125baf54557bec7e8a
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 01:04:58 +0300

    feat(stock): add stocks and vendors store with fetching capabilities

---
+++ app/src/store/services/stock.js
@@ -0,0 +1,91 @@
+import Collection from '@/lib/Collection';
+
+export const useStocksStore = defineStore('stocks', () => {
+   const useStocks = () => {
+      const defaultSelect = '*, product:products(*, category:product_category(*))';
+      const stocks = new Collection(null);
+
+      async function fetchStocks(select = defaultSelect) {
+         const res = await fetchStocks(select);
+         const { data } = res;
+         stocks._setDefaults(data || [])._reset();
+
+         return supabase.from('stocks').select(select).throwOnError();
+      }
+
+      async function fetchStock(id = '', select = defaultSelect) {
+         if (_isNil(id) || _isEmpty(id)) return;
+
+         const { data } = await supabase
+            .from('stocks')
+            .select(select)
+            .eq('id', id)
+            .single()
+            .throwOnError();
+
+         return data;
+      }
+
+      async function getStocks(select = defaultSelect) {
+         if (_isNil(stocks._data)) await fetchStocks(select);
+         return stocks;
+      }
+
+      async function getStock(id, select = defaultSelect) {
+         const stock = stocks ? stocks.find?.((stock) => stock.id === id) : null;
+         if (stock) {
+            return stock;
+         }
+         return await fetchStock(id, select);
+      }
+
+      return {
+         stocks,
+
+         fetchStocks,
+         fetchStock,
+         getStocks,
+         getStock
+      };
+   };
+
+   const useVendors = () => {
+      const vendors = new Collection(null);
+      const vendorStats = new Collection(null);
+
+      async function fetchVendors(select = '*') {
+         const { data } = await supabase.from('stock_vendors').select(select).throwOnError();
+         vendors._setDefaults(data || [])._reset();
+      }
+
+      async function fetchVendorStats(select = '*, product:products!product_id(*)') {
+         const { data } = await supabase.from('stock_vendor_stats').select(select).throwOnError();
+         return data;
+      }
+
+      async function getVendors(select) {
+         if (_isNil(vendors._data)) await fetchVendors(select);
+         return vendors;
+      }
+
+      async function getVendorStats(select) {
+         if (_isNil(vendorStats._data)) await fetchVendorStats(select);
+         return vendorStats;
+      }
+
+      return {
+         vendors,
+         vendorStats,
+
+         fetchVendors,
+         fetchVendorStats,
+         getVendors,
+         getVendorStats
+      };
+   };
+
+   return {
+      ...useStocks(),
+      ...useVendors()
+   };
+});
