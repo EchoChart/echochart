8b04b2b0b0a5be5ea9c9eab56da8412fdffba2a2
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 01:05:31 +0300

    feat(product): add PRODUCT_CATEGORY_PROPS for category styling and icons

---
+++ app/src/services/models/ProductModel/index.js
@@ -0,0 +1,5 @@
+export const PRODUCT_CATEGORY_PROPS = {
+   device: { class: '!bg-blue-600 !text-white !text-small !py-2', icon: 'pi pi-headphones' },
+   ['spare part']: { class: '!bg-red-600 !text-white !text-small !py-2', icon: PrimeIcons.WRENCH },
+   battery: { class: '!bg-yellow-600 !text-white !text-small !py-2', icon: PrimeIcons.BOLT }
+};
