041dab2e2f65ee0942abe5d3701629b5123f0bc1
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 01:03:59 +0300

    feat(product): add ProductBrandSelect component for brand selection with loading state

---
+++ app/src/components/service-components/product/ProductBrandSelect.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useProductsStore } from '@/store/services/product';
+
+const routeLoading = inject('routeLoading', false);
+
+const productStore = useProductsStore();
+const productBrands = routeLoading?.value ? [] : await productStore.getProductBrands();
+</script>
+
+<template>
+   <Select
+      :loading="routeLoading"
+      :filter="true"
+      :options="productBrands._data"
+      option-label="display_name"
+      option-value="display_name"
+   />
+</template>
