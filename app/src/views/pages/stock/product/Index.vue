177f41ec4ed8245917d9206c6137f82e06bd874a
AkifAycicek <akifaycicek@gmail.com>
2025-03-07 01:04:48 +0300

    feat(product): add product management page with upsert functionality and filters

---
+++ app/src/views/pages/stock/product/Index.vue
@@ -0,0 +1,149 @@
+<script setup>
+import Collection from '@/lib/Collection';
+import Upsert from './Upsert.vue';
+import { PRODUCT_CATEGORY_PROPS } from '@/services/models/ProductModel';
+
+defineOptions({
+   inheritAttrs: false
+});
+
+const attrs = useAttrs();
+const router = useRouter();
+const columns = new Collection([
+   {
+      field: 'display_name',
+      sortable: true,
+      header: i18n.t('product'),
+      sortOrder: { value: -1 }
+   },
+   {
+      field: 'brand',
+      sortable: true,
+      showFilterMatchModes: false,
+      showFilterOperator: false,
+      showAddButton: false,
+      header: i18n.t('brand')
+   },
+   {
+      field: 'categories.display_name',
+      showFilterMatchModes: false,
+      showFilterOperator: false,
+      showAddButton: false,
+      header: i18n.t('categories')
+   },
+   {
+      field: 'created_at',
+      sortable: true,
+      header: i18n.t('created_at'),
+      sortOrder: { value: -1 }
+   }
+]);
+
+// Define the filters object
+const filters = ref({
+   global: {
+      value: null,
+      matchMode: FilterMatchMode.CONTAINS
+   },
+   display_name: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }]
+   },
+   brand: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.CONTAINS }]
+   },
+   'categories.display_name': {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.IN }]
+   },
+   created_at: {
+      operator: FilterOperator.AND,
+      constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
+      dataType: 'date'
+   }
+});
+
+const stateKey = 'product';
+const rowActions = new Collection([
+   {
+      label: i18n.t('delete'),
+      command: async ({ data }) =>
+         await supabase
+            .from('products')
+            .delete()
+            .eq('id', data?.id)
+            .setHeader('item', JSON.stringify(data))
+            .throwOnError()
+            .then(() => emitter.emit(`${stateKey}-update`, data)),
+      icon: PrimeIcons.TRASH
+   },
+   {
+      label: i18n.t('edit'),
+      command: async ({ data }) =>
+         router.push({
+            name: 'product-edit',
+            params: {
+               id: data.id,
+               categories: data?.product?.categories
+                  ?.map?.(({ display_name }) => display_name)
+                  .join?.('|')
+            },
+            query: { showDialog: true }
+         }),
+      icon: PrimeIcons.PENCIL
+   }
+]);
+
+const dialogRef = inject('dialogRef', null);
+
+const tableProps = computed(() => ({
+   stateKey,
+   from: 'products',
+   select: '*, categories:product_category!inner(*)',
+   columns: columns._data,
+   rowActions: rowActions._data,
+   ...attrs
+}));
+</script>
+<template>
+   <ResourceTable v-bind="tableProps" v-model:filters="filters">
+      <template #header>
+         <div class="flex justify-end gap-4">
+            <KeywordSearchInput v-model="filters.global.value" />
+            <Teleport v-if="$can('create', 'products')" to="#page-toolbar" :disabled="dialogRef">
+               <div class="flex items-center justify-end gap-4">
+                  <CustomLink :to="{ name: 'product-add' }">
+                     <template #default="{ navigate }">
+                        <Button variant="outlined" :label="$t('add')" @click="navigate" />
+                     </template>
+                  </CustomLink>
+               </div>
+            </Teleport>
+         </div>
+      </template>
+      <template #brand_filter="{ filterModel }">
+         <ProductBrandSelect v-model="filterModel.value" editable />
+      </template>
+      <template #categories_display_name_filter="{ filterModel }">
+         <ProductCategorySelect v-model="filterModel.value" />
+      </template>
+      <template #categories_display_name_body="{ data }">
+         <div class="flex gap-2 flex-wrap justify-center">
+            <Badge
+               v-for="categories in data?.categories"
+               :value="$t(categories.display_name)"
+               size="large"
+               :key="categories.id"
+               v-bind="PRODUCT_CATEGORY_PROPS[categories.display_name]"
+            />
+         </div>
+      </template>
+      <template #expansion="{ data }">
+         <Upsert
+            :data
+            :categories="data?.categories?.map?.(({ display_name }) => display_name).join?.('|')"
+         />
+      </template>
+   </ResourceTable>
+</template>
---
+++ app/src/views/pages/stock/product/Upsert.vue
@@ -0,0 +1,185 @@
+<script setup>
+import { Form } from '@/lib/Form';
+import { useProductsStore } from '@/store/services/product';
+
+import { useToast } from 'primevue';
+
+const props = defineProps({
+   id: {
+      type: String,
+      default: null,
+      required: false
+   },
+   data: {
+      type: Object,
+      default: null,
+      required: false
+   }
+});
+
+const toast = useToast();
+
+const { ability, current_tenant_id } = useAuthStore();
+
+const initialFormData = {
+   id: undefined,
+   display_name: null,
+   brand: null,
+   description: null,
+   tenant_id: current_tenant_id,
+   categories: []
+};
+
+const fields = _keys(initialFormData);
+
+const form = new Form({
+   data: _defaults(_pick(props.data, fields), initialFormData),
+   rules: {
+      display_name: 'required',
+      brand: 'required',
+      categories: 'required'
+   },
+   useDialogForm: false
+});
+
+const readonly = computed(
+   () =>
+      (ability.cannot('modify', 'products') && ability.cannot('create', 'products')) ||
+      (form.id && !form.tenant_id)
+);
+
+const { getProducts } = useProductsStore();
+
+if (props.id) {
+   getProducts().then((products) => {
+      const product = products?.find?.((product) => product.id === props.id);
+      form._setDefaults(_pick(product, fields))._reset();
+   });
+}
+
+const save = async () => {
+   if (!form._validate()) return;
+
+   const payload = _omit(form._data, ['categories']);
+
+   if (form.id)
+      await supabase
+         .from('product_categories')
+         .delete()
+         .eq('product_id', form.id)
+         .setHeader('x-delete-confirmed', true)
+         .throwOnError();
+
+   await supabase
+      .from('products')
+      .upsert(payload)
+      .eq('id', form.id)
+      .select()
+      .single()
+      .throwOnError()
+      .then(({ data }) => form._merge(_pick(data, fields)));
+
+   await supabase
+      .from('product_categories')
+      .insert(
+         form.categories.map(({ id: category_id }) => ({
+            category_id,
+            product_id: form.id
+         }))
+      )
+      .throwOnError();
+
+   emitter.emit('product-update', form._data);
+
+   form._setDefaults(form._data)._reset();
+
+   toast.add({
+      life: 3000,
+      severity: ToastSeverity.SUCCESS,
+      summary: i18n.t('success'),
+      detail: i18n.t('saved')
+   });
+};
+
+if (props.id || props.data?.id) {
+   const updateCallback = (data) => form._setDefaults(_pick(data, fields))._reset();
+   onMounted(() => emitter.on('product-update', updateCallback));
+   onUnmounted(() => emitter.off('product-update', updateCallback));
+}
+</script>
+
+<template>
+   <div class="card">
+      <FormBox @submit="save" @reset="() => form._reset()" class="[&>*]:flex-1">
+         <FormField
+            class="flex-[0.8]"
+            :readonly="readonly"
+            fluid
+            :error="form._errors.first('display_name')"
+            :label="$t('display_name')"
+         >
+            <template #default="slotProps">
+               <InputText class="min-w-24" v-bind="slotProps" v-model="form.display_name" />
+            </template>
+         </FormField>
+         <FormField
+            class="flex-[0.8]"
+            :readonly="readonly"
+            fluid
+            :error="form._errors.first('brand')"
+            :label="$t('brand')"
+         >
+            <template #default="slotProps">
+               <InputText class="min-w-24" v-bind="slotProps" v-model="form.brand" />
+            </template>
+         </FormField>
+         <FormField
+            class="flex-[0.8]"
+            :readonly="readonly"
+            fluid
+            :error="form._errors.first('categories')"
+            :label="$t('categories')"
+         >
+            <template #default="slotProps">
+               <ProductCategorySelect
+                  v-bind="slotProps"
+                  v-model="form.categories"
+                  :disabled="readonly"
+                  :option-value="null"
+               />
+            </template>
+         </FormField>
+         <div class="!flex-auto w-full">
+            <FormField
+               class="flex-[0.8]"
+               :readonly="readonly"
+               fluid
+               :error="form._errors.first('description')"
+               :label="$t('description')"
+            >
+               <template #default="slotProps">
+                  <Editor v-bind="slotProps" v-model="form.description" />
+               </template>
+            </FormField>
+         </div>
+         <div
+            v-if="$can('create', 'products') || $can('modify', 'products')"
+            class="flex flex-wrap items-end justify-end gap-4 !flex-auto w-full"
+         >
+            <Button
+               :label="$t('save')"
+               class="flex-[.2]"
+               :disabled="!form?._isChanged"
+               type="submit"
+            />
+            <Button
+               :label="$t('reset')"
+               severity="secondary"
+               class="flex-[.2]"
+               :disabled="!form?._isChanged"
+               type="reset"
+            />
+         </div>
+      </FormBox>
+   </div>
+</template>
