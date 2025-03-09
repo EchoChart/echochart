6f9dd16ff2a3d0807ce4c949fe1af2bf29d0c4c6
AkifAycicek <akifaycicek@gmail.com>
2025-03-09 20:55:26 +0300

    feat: enhance ProductSelect component with edit and add functionality, and improve props handling

--- app/src/components/service-components/product/ProductSelect.vue
+++ app/src/components/service-components/product/ProductSelect.vue
@@ -5,8 +5,22 @@ const props = defineProps({
       type: String,
       default: null,
       required: false
+   },
+   showEdit: {
+      type: Boolean,
+      default: true
+   },
+   showAdd: {
+      type: Boolean,
+      default: true
    }
 });
+
+defineOptions({
+   inheritAttrs: false
+});
+
+const modelValue = defineModel('modelValue');
 
 const routeLoading = inject('routeLoading', false);
 
@@ -18,17 +32,43 @@ const productsByCategory = routeLoading?.value ? [] : await productStore.getProd
 
 const products = computed(() => {
    if (categories.value.some?.((c) => _keys(productsByCategory._data).includes(c)))
-      return _flatMap(_values(_pick(productsByCategory, categories.value)));
+      return _flatMap(_values(_pick(productsByCategory._data, categories.value)));
    return allProducts._data;
 });
 </script>
 
 <template>
-   <Select
-      :loading="routeLoading"
-      :filter="true"
-      :options="products"
-      option-label="display_name"
-      option-value="id"
-   />
+   <span class="flex gap-2 items-center" :class="$attrs?.class">
+      <Select
+         :loading="routeLoading"
+         :filter="true"
+         :options="products"
+         option-label="display_name"
+         option-value="id"
+         v-bind="$attrs"
+         v-model:model-value="modelValue"
+      />
+      <RouterLink
+         v-if="showEdit && $can('modify', 'products') && !!modelValue"
+         :to="{
+            name: 'product-edit',
+            params: { id: modelValue },
+            query: { showDialog: 'center' }
+         }"
+         v-slot="{ navigate }"
+      >
+         <Button size="small" severity="info" :icon="PrimeIcons.PENCIL" @click="navigate" />
+      </RouterLink>
+      <RouterLink
+         v-if="showAdd && $can('create', 'products')"
+         :to="{
+            name: 'product-add',
+            params: { category },
+            query: { showDialog: 'center' }
+         }"
+         v-slot="{ navigate }"
+      >
+         <Button size="small" severity="success" :icon="PrimeIcons.PLUS" @click="navigate" />
+      </RouterLink>
+   </span>
 </template>
