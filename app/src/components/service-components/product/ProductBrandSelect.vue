1e425141ce0a642153c21ff2c42b44b1a80d3c6c
AkifAycicek <akifaycicek@gmail.com>
2025-03-10 16:39:00 +0300

    refactor: remove route loading dependency and simplify product brand fetching

--- app/src/components/service-components/product/ProductBrandSelect.vue
+++ app/src/components/service-components/product/ProductBrandSelect.vue
@@ -1,15 +1,12 @@
 <script setup>
 import { useProductsStore } from '@/store/services/product';
 
-const routeLoading = inject('routeLoading', false);
-
 const productStore = useProductsStore();
-const productBrands = routeLoading?.value ? [] : await productStore.getProductBrands();
+const productBrands = await productStore.getProductBrands();
 </script>
 
 <template>
    <Select
-      :loading="routeLoading"
       :filter="true"
       :options="productBrands._data"
       option-label="display_name"
