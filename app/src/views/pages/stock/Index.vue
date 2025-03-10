c88e7d8d3751460c1d79430f84198aec8a61d7a4
AkifAycicek <akifaycicek@gmail.com>
2025-03-10 16:12:39 +0300

    feat: add stock page with dynamic routing and menu bar integration

---
+++ app/src/views/pages/stock/Index.vue
@@ -0,0 +1,45 @@
+<script setup>
+const router = useRouter();
+onBeforeRouteUpdate?.(async (to) => {
+   if (!to?.query?.showDialog && to.name == 'stock') {
+      return await router.replace({ name: 'device-list' });
+   }
+});
+
+const { routes, breakpoints } = useLayout();
+const tabs = computed(() => routes.value.find((item) => item.route?.name == 'stock')?.items);
+const dialogRef = inject('dialogRef', null);
+const mounted = useMounted();
+</script>
+<template>
+   <div class="card flex flex-col gap-4">
+      <Teleport
+         v-if="mounted && dialogRef"
+         to="#page-toolbar"
+         defer
+         :disabled="!breakpoints.lg || dialogRef"
+      >
+         <MenuBar v-if="mounted" :model="tabs" :breakpoint="'0px'">
+            <template #item="{ item, props, hasSubmenu }">
+               <span
+                  :class="{
+                     '!text-primary-emphasis hover:!text-current':
+                        !dialogRef && $route.matched.some(({ name }) => name === item.route?.name)
+                  }"
+               >
+                  <CustomLink v-if="!hasSubmenu" :to="item.route" class="p-menubar-item-link">
+                     <span class="p-menubar-item-icon !text-inherit" :class="item.icon" />
+                     <span class="p-menubar-item-label" v-text="$t(item.label)" />
+                  </CustomLink>
+                  <div v-else v-ripple v-bind="props.action">
+                     <span :class="item.icon" />
+                     <span v-text="$t(item.label)" />
+                     <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+                  </div>
+               </span>
+            </template>
+         </MenuBar>
+      </Teleport>
+      <CustomRouteView class="flex-1" />
+   </div>
+</template>
