e678013090c17bd14725123bed57475f3041a659
AkifAycicek <akifaycicek@gmail.com>
2025-03-11 15:44:55 +0300

    refactor: simplify Access.vue template by removing commented code and unnecessary elements

--- app/src/views/pages/auth/Access.vue
+++ app/src/views/pages/auth/Access.vue
@@ -3,25 +3,13 @@ const dialogRef = inject('dialogRef', null);
 </script>
 
 <template>
-   <div class="min-h-full flex flex-col items-center justify-center">
-      <!-- <div
-         class="p-1 rounded-[calc(var(--content-border-radius)*6)] bg-gradient-to-b from-[rgba(247,149,48,0.4)] via-transparent to-transparent"
-      > -->
+   <div class="min-h-full flex flex-col items-center">
       <div
          class="w-full rounded-[inherit] bg-surface-0 dark:bg-surface-900 p-20 flex flex-col items-center"
       >
          <div class="gap-8 flex flex-col items-center">
-            <div
-               class="flex justify-center items-center border-2 border-orange-500 rounded-full"
-               style="width: 3.2rem; height: 3.2rem"
-            >
-               <i class="text-orange-500 pi pi-fw pi-lock !text-2xl"></i>
-            </div>
-            <h1 class="text-surface-900 dark:text-surface-0 font-bold text-4xl lg:text-5xl">
-               Access Denied
-            </h1>
             <img
-               src="/demo/images/access/you-shall-not-pass.png"
+               src="/images/access/you-shall-not-pass.png"
                alt="Access denied"
                class="max-h-[28rem]"
             />
@@ -35,6 +23,5 @@ const dialogRef = inject('dialogRef', null);
             </div>
          </div>
       </div>
-      <!-- </div> -->
    </div>
 </template>
