3480290f1127f6733bcfaa698e9ff824037c17bd
AkifAycicek <akifaycicek@gmail.com>
2025-04-18 22:09:32 +0300

    refactor: revert class properties to severity for consistency in PRODUCT_CATEGORY_PROPS

--- app/src/services/models/ProductModel/index.js
+++ app/src/services/models/ProductModel/index.js
@@ -1,5 +1,5 @@
 export const PRODUCT_CATEGORY_PROPS = {
-   device: { class: '!bg-blue-600 !text-white !text-small !py-2', icon: 'pi pi-headphones' },
-   ['spare part']: { class: '!bg-red-600 !text-white !text-small !py-2', icon: PrimeIcons.WRENCH },
-   battery: { class: '!bg-yellow-600 !text-white !text-small !py-2', icon: PrimeIcons.BOLT }
+   device: { severity: 'danger', icon: 'pi pi-headphones' },
+   ['spare part']: { severity: 'info', icon: PrimeIcons.WRENCH },
+   battery: { severity: 'success', icon: PrimeIcons.BOLT }
 };
