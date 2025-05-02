b5b7a462ce23fd87c06f6a110b7a1ceb98e5a266
AkifAycicek <akifaycicek@gmail.com>
2025-05-02 17:01:29 +0300

    fix: conditionally render RouteMenuBar based on dialogRef in branch and stock views

--- app/src/views/pages/branch/Index.vue
+++ app/src/views/pages/branch/Index.vue
@@ -1,10 +1,11 @@
 <script setup>
 const { routes } = useLayout();
 const menuRoute = computed(() => routes.value.find((item) => item.route?.name == 'branch'));
+const dialogRef = inject('dialogRef', false);
 </script>
 <template>
    <div class="card p-0 flex flex-col gap-4">
-      <RouteMenuBar :route="menuRoute" />
+      <RouteMenuBar v-if="dialogRef" :route="menuRoute" />
       <CustomRouteView class="flex-1" />
    </div>
 </template>
