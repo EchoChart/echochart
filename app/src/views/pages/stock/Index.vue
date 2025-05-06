b1889bf6f26f8b6c76b8fc5450b4b055f9e0e951
AkifAycicek <akifaycicek@gmail.com>
2025-05-06 13:07:34 +0300

    fix: update RouteMenuBar rendering condition in branch and stock views for improved logic

--- app/src/views/pages/stock/Index.vue
+++ app/src/views/pages/stock/Index.vue
@@ -5,7 +5,7 @@ const dialogRef = inject('dialogRef', false);
 </script>
 <template>
    <div class="card p-0 flex flex-col gap-4">
-      <RouteMenuBar v-if="dialogRef" :route="menuRoute" />
+      <RouteMenuBar v-if="dialogRef || $route.name === menuRoute.name" :route="menuRoute" />
       <CustomRouteView class="flex-1" />
    </div>
 </template>
