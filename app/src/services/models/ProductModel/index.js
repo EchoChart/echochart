762d2019e4c56782f912bd4f763504d9f6a12657
AkifAycicek <akifaycicek@gmail.com>
2025-05-06 13:53:14 +0300

    fix: update severity level for device category in PRODUCT_CATEGORY_PROPS

--- app/src/services/models/ProductModel/index.js
+++ app/src/services/models/ProductModel/index.js
@@ -1,5 +1,5 @@
 export const PRODUCT_CATEGORY_PROPS = {
-   device: { severity: 'danger', icon: 'pi pi-headphones' },
+   device: { severity: 'warn', icon: 'pi pi-headphones' },
    ['spare part']: { severity: 'info', icon: PrimeIcons.WRENCH },
    battery: { severity: 'success', icon: PrimeIcons.BOLT }
 };
