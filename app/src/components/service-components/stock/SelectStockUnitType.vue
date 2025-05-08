fca1a75213fee5a7b7892cb09754cbc775a1c0bd
AkifAycicek <akifaycicek@gmail.com>
2025-05-08 20:38:11 +0300

    feat: add SelectStockUnitType component for unit type selection in stock management

---
+++ app/src/components/service-components/stock/SelectStockUnitType.vue
@@ -0,0 +1,53 @@
+<script setup>
+const unitTypes = computed(() => [
+   {
+      label: i18n.t('pcs'),
+      value: 'pcs'
+   },
+   {
+      label: i18n.t('kg'),
+      value: 'kg'
+   },
+   {
+      label: i18n.t('gr'),
+      value: 'gr'
+   },
+   {
+      label: i18n.t('lbs'),
+      value: 'lbs'
+   },
+   {
+      label: i18n.t('L'),
+      value: 'L'
+   },
+   {
+      label: i18n.t('mL'),
+      value: 'mL'
+   },
+   {
+      label: i18n.t('m'),
+      value: 'm'
+   },
+   {
+      label: i18n.t('m²'),
+      value: 'm²'
+   },
+   {
+      label: i18n.t('cm'),
+      value: 'cm'
+   },
+   {
+      label: i18n.t('in'),
+      value: 'in'
+   }
+]);
+</script>
+<template>
+   <Select
+      :filter="true"
+      option-label="label"
+      option-value="value"
+      :options="unitTypes"
+      :placeholder="$t('select_unit_type')"
+   />
+</template>
