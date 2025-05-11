4c74f891baf0e68f66fdf49363465ab5d5f4185f
AkifAycicek <akifaycicek@gmail.com>
2025-05-11 22:50:28 +0300

    fix: update FormBox component to handle legend prop as null and improve form structure

--- app/src/components/ui/FormBox.vue
+++ app/src/components/ui/FormBox.vue
@@ -4,7 +4,7 @@ import { Form } from '@/lib/Form';
 defineProps({
    legend: {
       type: String,
-      default: ''
+      default: null
    },
    form: {
       type: Form,
@@ -18,42 +18,46 @@ defineProps({
 </script>
 
 <template>
-   <Fieldset
-      v-if="legend"
-      :legend="legend"
-      :pt="{
-         content: {
-            class: 'form-box'
-         }
-      }"
-   >
-      <slot />
-   </Fieldset>
-   <form v-else class="form-box" @submit.prevent>
-      <slot :readonly :form />
+   <template v-if="_isNil(legend)">
+      <form class="form-box" @submit.prevent>
+         <slot :readonly :form />
 
-      <slot name="form-actions" :form :readonly :legend>
-         <div v-if="!readonly" class="flex flex-wrap items-center gap-4 !flex-auto w-full">
-            <Button
-               :label="$t('save')"
-               class="flex-[.2] w-48 ms-auto"
-               :disabled="!form?._isChanged"
-               type="submit"
-            />
-            <Button
-               :label="$t('reset')"
-               severity="secondary"
-               class="flex-[.2] w-48"
-               :disabled="!form?._isChanged"
-               type="reset"
-            />
-         </div>
-      </slot>
-   </form>
+         <slot name="form-actions" :form :readonly :legend>
+            <div v-if="!readonly" class="flex flex-wrap items-center gap-4 !flex-auto w-full">
+               <Button
+                  :label="$t('save')"
+                  class="flex-[.2] w-48 ms-auto"
+                  :disabled="!form?._isChanged"
+                  type="submit"
+               />
+               <Button
+                  :label="$t('reset')"
+                  severity="secondary"
+                  class="flex-[.2] w-48"
+                  :disabled="!form?._isChanged"
+                  type="reset"
+               />
+            </div>
+         </slot>
+      </form>
+   </template>
+   <template v-else>
+      <Fieldset
+         :legend="_startCase(legend)"
+         :pt="{
+            content: {
+               class: 'form-box'
+            }
+         }"
+         class="form-box"
+      >
+         <slot />
+      </Fieldset>
+   </template>
 </template>
 <style lang="scss">
 .form-box {
-   @apply flex-1 flex flex-wrap gap-2;
+   @apply flex-1 flex flex-wrap gap-2 items-start;
 
    & .form-field {
       @apply flex-1 min-w-min;
