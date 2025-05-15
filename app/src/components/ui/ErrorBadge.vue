d3c84a617bae7a8665048accef6b80763490f58a
AkifAycicek <akifaycicek@gmail.com>
2025-05-15 15:13:08 +0300

    feat: add ErrorBadge component with tooltip for error display

---
+++ app/src/components/ui/ErrorBadge.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+   error: {
+      type: String,
+      default: null
+   }
+});
+</script>
+<template>
+   <Badge
+      severity="danger"
+      class="!p-0"
+      v-tooltip="{
+         value: error,
+         pt: {
+            root: {
+               class: 'backdrop-blur-md'
+            },
+            text: '!bg-red-400 !bg-opacity-20 !border-red-400 border !text-current '
+         }
+      }"
+   >
+      <i class="!text-lg" :class="PrimeIcons.EXCLAMATION_CIRCLE" />
+   </Badge>
+</template>
