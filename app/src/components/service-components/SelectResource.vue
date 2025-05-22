63f7f556c8b399593fb0a680f222d0f3e8033cbe
AkifAycicek <akifaycicek@gmail.com>
2025-05-22 18:31:52 +0300

    feat: add SelectResource component for resource selection dialog

---
+++ app/src/components/service-components/SelectResource.vue
@@ -0,0 +1,54 @@
+<script setup>
+import ResourceTable from '@components/service-components/ResourceTable.vue';
+import { useDialog } from 'primevue';
+
+defineOptions({
+   inheritAttrs: false
+});
+
+const emit = defineEmits(['dialog']);
+const props = defineProps({
+   optionLabel: {
+      type: String,
+      default: null
+   },
+   optionValue: {
+      type: String,
+      default: 'id'
+   },
+   tableProps: {
+      type: Object,
+      default: () => ({})
+   }
+});
+const attrs = useAttrs();
+const slots = useSlots();
+
+let dialog = null;
+const dialogs = useDialog();
+
+const tableProps = reactive(_merge({}, props.tableProps));
+const selectResource = () => {
+   dialog = dialogs.open(h(ResourceTable, tableProps, slots), {
+      props: {
+         header: i18n.t('select_item'),
+         class: '!min-w-[clamp(32rem,50%,100vw)] !max-w-min'
+      }
+   });
+   emit('dialog', dialog);
+};
+watch(
+   () => props.tableProps,
+   (value) => _merge(tableProps, value),
+   { deep: true }
+);
+</script>
+<template>
+   <slot />
+   <InputGroup :class="attrs.class">
+      <InputText v-bind="attrs" readonly />
+      <InputGroupAddon>
+         <Button :icon="PrimeIcons.SEARCH_PLUS" @click="selectResource" />
+      </InputGroupAddon>
+   </InputGroup>
+</template>
