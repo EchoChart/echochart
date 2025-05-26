ccad9dc44aea663daae478d9edf935a7bc631e63
AkifAycicek <akifaycicek@gmail.com>
2025-05-26 21:44:36 +0300

    refactor: improve styling and class names in RouteMenuBar component for better readability and maintainability

--- app/src/components/ui/route-menu-bar/RouteMenuBar.vue
+++ app/src/components/ui/route-menu-bar/RouteMenuBar.vue
@@ -10,29 +10,48 @@ const mounted = useMounted();
 const dialogRef = inject('dialogRef', null);
 const tabs = computed(() => props.route?.items);
 </script>
+
 <template>
    <Teleport v-if="mounted" to="#page-toolbar" defer :disabled="dialogRef">
       <MenuBar v-if="mounted" :model="tabs" :breakpoint="'0px'">
          <template #item="{ item, props, hasSubmenu }">
             <span
                :class="{
-                  '!text-primary-emphasis hover:!text-current':
+                  'route-menu-bar__item':
                      !dialogRef && $route.matched.some(({ name }) => name === item.route?.name)
                }"
             >
                <CustomLink v-if="!hasSubmenu" :to="item.route" v-slot="{ navigate }">
-                  <span class="p-menubar-item-link" @click="navigate">
-                     <span class="p-menubar-item-icon !text-inherit" :class="item.icon" />
-                     <span class="p-menubar-item-label" v-text="$t(item.label)" />
+                  <span class="route-menu-bar__link p-menubar-item-link" @click="navigate">
+                     <span class="route-menu-bar__icon p-menubar-item-icon" :class="item.icon" />
+                     <span
+                        class="route-menu-bar__label p-menubar-item-label"
+                        v-text="$t(item.label)"
+                     />
                   </span>
                </CustomLink>
                <div v-else v-ripple v-bind="props.action">
                   <span :class="item.icon" />
                   <span v-text="$t(item.label)" />
-                  <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+                  <span
+                     v-if="hasSubmenu"
+                     class="route-menu-bar__submenu-indicator pi pi-fw pi-angle-down"
+                  />
                </div>
             </span>
          </template>
       </MenuBar>
    </Teleport>
 </template>
+
+<style lang="scss">
+.route-menu-bar {
+   &__item {
+      @apply text-primary-emphasis hover:text-current;
+   }
+
+   &__icon {
+      @apply text-inherit !important;
+   }
+}
+</style>
