6d2a2d4b64dedbae7f8919109922654ef10212bf
AkifAycicek <akifaycicek@gmail.com>
2025-05-26 22:26:38 +0300

    refactor: update ActionSpeedDial component class names for improved readability and maintainability

--- app/src/components/ui/custom-table/ActionSpeedDial.vue
+++ app/src/components/ui/custom-table/ActionSpeedDial.vue
@@ -4,15 +4,29 @@
    <SpeedDial
       :transitionDelay="40"
       direction="right"
-      class="justify-self-start"
+      class="custom-table__action-speeddial-container"
       @mouseleave="(e) => e?.target?.classList?.remove?.('p-speeddial-open')"
       :buttonProps="{
          severity: 'secondary',
          size: 'small',
          rounded: true,
-         class: '!w-10 !h-10 self-center',
+         class: 'custom-table__action-speeddial-button',
          onMouseenter: (e) => e?.target?.parentElement?.classList?.toggle?.('p-speeddial-open')
       }"
       :tooltipOptions="{ position: 'top' }"
    />
 </template>
+
+<style lang="scss">
+.custom-table {
+   &__action-speeddial {
+      &-container {
+         @apply justify-self-start;
+      }
+
+      &-button {
+         @apply !w-10 !h-10 self-center;
+      }
+   }
+}
+</style>
