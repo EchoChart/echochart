80d3b28093ba148cc03c4983f789edbb043b19af
AkifAycicek <akifaycicek@gmail.com>
2025-05-27 02:26:54 +0300

    refactor: move STOCK_UNIT_TYPES to a separate constants file for improved organization and maintainability

---
+++ app/src/constants/form/stock.js
@@ -0,0 +1,42 @@
+export const STOCK_UNIT_TYPES = [
+   {
+      label: i18n.t('pcs'),
+      value: 'pcs'
+   },
+   {
+      label: i18n.t('kg'),
+      value: 'kg'
+   },
+   {
+      label: i18n.t('gr'),
+      value: 'gr'
+   },
+   {
+      label: i18n.t('lbs'),
+      value: 'lbs'
+   },
+   {
+      label: i18n.t('L'),
+      value: 'L'
+   },
+   {
+      label: i18n.t('mL'),
+      value: 'mL'
+   },
+   {
+      label: i18n.t('m'),
+      value: 'm'
+   },
+   {
+      label: i18n.t('m²'),
+      value: 'm²'
+   },
+   {
+      label: i18n.t('cm'),
+      value: 'cm'
+   },
+   {
+      label: i18n.t('in'),
+      value: 'in'
+   }
+];
