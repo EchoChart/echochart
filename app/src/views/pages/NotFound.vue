4ecc6abb1c772b096a3ff597eecea1cbd3062a47
AkifAycicek <akifaycicek@gmail.com>
2025-05-27 13:03:57 +0300

    refactor: improve NotFound page structure and styles for better readability and organization

--- app/src/views/pages/NotFound.vue
+++ app/src/views/pages/NotFound.vue
@@ -1,19 +1,12 @@
 <template>
-   <div class="flex-1 flex justify-center">
-      <div class="flex flex-col items-center justify-center">
-         <div
-            class="p-1 m-auto rounded-[calc(var(--content-border-radius)*6)] bg-gradient-to-b from-[rgba(233,30,99,0.4)] via-transparent to-transparent"
-         >
-            <div
-               class="rounded-[inherit] bg-surface-0 dark:bg-surface-900 p-14 flex flex-col gap-4 items-center"
-            >
-               <span class="text-red-500 font-bold text-3xl" v-text="'404'" />
-               <h1
-                  class="text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2"
-                  v-text="$t('not_found')"
-               />
+   <div class="not_found_page__container">
+      <div class="not_found_page__content">
+         <div class="not_found_page__gradient-bg">
+            <div class="not_found_page__inner-content">
+               <span class="not_found_page__error-code" v-text="'404'" />
+               <h1 class="not_found_page__title" v-text="$t('not_found')" />
                <div
-                  class="text-surface-600 dark:text-surface-200 mb-8"
+                  class="not_found_page__description"
                   v-text="$t('requested_resource_is_not_available')"
                />
                <img src="/images/error/asset-error.svg" alt="Access denied" width="80%" />
@@ -25,3 +18,35 @@
       </div>
    </div>
 </template>
+
+<style lang="scss">
+.not_found_page {
+   &__container {
+      @apply flex-1 flex justify-center;
+   }
+
+   &__content {
+      @apply flex flex-col items-center justify-center;
+   }
+
+   &__gradient-bg {
+      @apply p-1 m-auto rounded-[calc(var(--content-border-radius)*6)] bg-gradient-to-b from-[rgba(233,30,99,0.4)] via-transparent to-transparent;
+   }
+
+   &__inner-content {
+      @apply rounded-[inherit] bg-surface-0 dark:bg-surface-900 p-14 flex flex-col gap-4 items-center;
+   }
+
+   &__error-code {
+      @apply text-red-500 font-bold text-3xl;
+   }
+
+   &__title {
+      @apply text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2;
+   }
+
+   &__description {
+      @apply text-surface-600 dark:text-surface-200 mb-8;
+   }
+}
+</style>
