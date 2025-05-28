13b56a2824701c3326805ec191cdc96062817d80
AkifAycicek <akifaycicek@gmail.com>
2025-05-28 13:30:07 +0300

    refactor: restructure FloatingConfigurator component for improved readability and maintainability

--- app/src/components/layout/FloatingConfigurator.vue
+++ app/src/components/layout/FloatingConfigurator.vue
@@ -5,10 +5,30 @@ const appConfigurator = ref();
 </script>
 
 <template>
-   <div class="fixed flex gap-4 top-8 right-8">
-      <Button icon="pi pi-palette" @click="appConfigurator?.toggle" type="button" rounded />
-      <Popover class="w-min" ref="appConfigurator">
+   <div class="floating-configurator">
+      <Button
+         icon="pi pi-palette"
+         @click="appConfigurator?.toggle"
+         type="button"
+         rounded
+         class="floating-configurator__button"
+      />
+      <Popover class="floating-configurator__popover" ref="appConfigurator">
          <AppConfigurator />
       </Popover>
    </div>
 </template>
+
+<style lang="scss">
+.floating-configurator {
+   @apply fixed flex gap-4 top-8 right-8;
+
+   &__button {
+      @apply rounded;
+   }
+
+   &__popover {
+      @apply w-min;
+   }
+}
+</style>
