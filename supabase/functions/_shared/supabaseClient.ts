d1b705bdc25108e7010ecca8ac340d249188fccf
AkifAycicek <akifaycicek@gmail.com>
2025-07-18 17:07:19 +0300

    refactor(supabase): update client to use new Supabase JS library and improve type safety
    - Updated `supabaseClient` and `supabaseAdmin` to use the latest version of `@supabase/supabase-js`
    - Added generics for better type safety in `SupabaseClientOptions` and return types
    - Replaced service role key with anon key for improved security practices
    - Refactored function signatures to allow optional configuration options

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -1,12 +1,37 @@
-import { createClient } from "jsr:@supabase/supabase-js@2.47.10";
-export const supabase = () =>
-  createClient(
-    Deno.env.get("SUPABASE_URL") ?? "",
-    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
-    {
-      auth: {
-        autoRefreshToken: false,
-        persistSession: false,
-      },
-    }
-  );
+import { createClient, SupabaseClientOptions } from 'npm:@supabase/supabase-js';
+import type { Database } from './database.types.ts';
+export const supabaseClient = <
+   DB = Database,
+   SchemaName extends string & keyof DB = 'public' extends keyof DB ? 'public' : string & keyof DB
+>(
+   options?: SupabaseClientOptions<SchemaName>
+) =>
+   createClient<DB, SchemaName>(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      {
+         auth: {
+            autoRefreshToken: false,
+            persistSession: false
+         },
+         ...options
+      }
+   );
+
+export const supabaseAdmin = <
+   DB = Database,
+   SchemaName extends string & keyof DB = 'public' extends keyof DB ? 'public' : string & keyof DB
+>(
+   options?: SupabaseClientOptions<SchemaName>
+) =>
+   createClient<DB, SchemaName>(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
+      {
+         auth: {
+            autoRefreshToken: false,
+            persistSession: false
+         },
+         ...options
+      }
+   );
